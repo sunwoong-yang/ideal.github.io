1be40ba318a49e598d2442a88ef690a11690474f
sunwoong-yang <sunwoongy@gmail.com>
2025-12-18 23:45:23 +0900

    initial project

--- js/main.js
+++ js/main.js
@@ -109,23 +109,23 @@
 	};
 	carousel();
 
-	$('nav .dropdown').hover(function(){
-		var $this = $(this);
-		// 	 timer;
-		// clearTimeout(timer);
-		$this.addClass('show');
-		$this.find('> a').attr('aria-expanded', true);
-		// $this.find('.dropdown-menu').addClass('animated-fast fadeInUp show');
-		$this.find('.dropdown-menu').addClass('show');
-	}, function(){
-		var $this = $(this);
-			// timer;
-		// timer = setTimeout(function(){
-			$this.removeClass('show');
-			$this.find('> a').attr('aria-expanded', false);
-			// $this.find('.dropdown-menu').removeClass('animated-fast fadeInUp show');
-			$this.find('.dropdown-menu').removeClass('show');
-		// }, 100);
+	$('nav .dropdown').each(function(){
+		var timeout;
+		$(this).hover(function(){
+			var $this = $(this);
+			clearTimeout(timeout);
+			$this.addClass('show');
+			$this.find('> a').attr('aria-expanded', true);
+			$this.find('.dropdown-menu').addClass('show');
+		}, function(){
+			var $this = $(this);
+			clearTimeout(timeout);
+			timeout = setTimeout(function(){
+				$this.removeClass('show');
+				$this.find('> a').attr('aria-expanded', false);
+				$this.find('.dropdown-menu').removeClass('show');
+			}, 1000);
+		});
 	});
 
 
